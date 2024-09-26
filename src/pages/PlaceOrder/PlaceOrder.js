d60d8384446a28030d8a286a5594a22a32a021a0
kavi <vettrithamil@gmail.com>
2024-09-26 13:06:34 +0530

    completed the checkout page

--- src/pages/PlaceOrder/PlaceOrder.js
+++ src/pages/PlaceOrder/PlaceOrder.js
@@ -1,10 +1,59 @@
-import React from 'react'
-import './PlaceOrder.css'
+import React, { useContext } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../context/store_context";
+import { useNavigate } from "react-router-dom";
 
 const PlaceOrder = () => {
-  return (
-    <div>PlaceOrder</div>
-  )
-}
 
-export default PlaceOrder
+  const {getTotalCartAmount} = useContext(StoreContext);
+  const navigate = useNavigate();
+
+  return (
+    <div className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-field">
+          <input type="text" placeholder="First name" />
+          <input type="text" placeholder="Second name" />
+        </div>
+        <input type="email" placeholder="Email address" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-field">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-field">
+          <input type="text" placeholder="Zip code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="Country" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>₹{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>₹{getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>₹{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button onClick={() => navigate("/order")}>
+            PROCEED TO PAYMENT
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlaceOrder;
